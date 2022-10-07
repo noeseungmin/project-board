709ccb62d72429a0bc4670ee6063fc930a03ca71
seungmin <nowinmin96@gmail.com>
2022-10-07 22:30:23 +0900

    #10 - repository interface 작성
    이걸로 db에 접근

---
+++ src/main/java/com/seungmin/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.seungmin.projectboard.repository;
+
+import com.seungmin.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
