b6512bc417538063b663c75de81e2fddda0d813d
seungmin <nowinmin96@gmail.com>
2022-10-08 16:58:57 +0900

    #1 - datarest 설정
    게시글 댓글 json api를 자동으로 restful 하게 만들게끔 설정

--- src/main/java/com/seungmin/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/seungmin/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.seungmin.projectboard.repository;
 
 import com.seungmin.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
