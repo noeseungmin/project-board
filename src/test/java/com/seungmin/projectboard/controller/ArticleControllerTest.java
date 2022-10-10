0001f26ca5830b095753d7e1972b97f7fd9df200
seungmin <nowinmin96@gmail.com>
2022-10-10 20:41:31 +0900

    #16 - 로그인 컨트롤러 테스트  + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 `/login`컨트롤러 테스트 그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 `@import` 추가

--- src/test/java/com/seungmin/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/seungmin/projectboard/controller/ArticleControllerTest.java
@@ -1,11 +1,13 @@
 package com.seungmin.projectboard.controller;
 
+import com.seungmin.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -49,7 +52,7 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articleComments"));
     }
 
-    @Disabled("개발중")
+
     @DisplayName("[View][GET] 게시글 검색 전용 페이지 정상 호출")
     @Test
     public void givenNothing_whenRequestArticleSearch_thanReturnView() throws Exception {
@@ -60,7 +63,7 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/search"));
     }
 
-    @Disabled("개발중")
+
     @DisplayName("[View][GET] 게시글 해시태그 검색 페이지 정상 호출")
     @Test
     public void givenNothing_whenRequestArticleHashtagSearch_thanReturnView() throws Exception {
